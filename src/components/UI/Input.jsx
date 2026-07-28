e362978a022a1c13cae5ea4219db60e01d52e8dd
ꜱɢ ᴅᴀʀᴋ 神 <godwithusmustgr8@gmail.com>
2026-07-28 16:32:08 +0100

    Update Input.jsx

--- src/components/UI/Input.jsx
+++ src/components/UI/Input.jsx
@@ -1,1 +1,18 @@
+import React from 'react';
 
+export default function Input({ label, value, onChange, icon, type = 'text', placeholder }) {
+  return (
+    <div className="form-group">
+      {label && <label>{label}</label>}
+      <div className="input-with-icon">
+        {icon}
+        <input
+          type={type}
+          value={value}
+          onChange={onChange}
+          placeholder={placeholder}
+        />
+      </div>
+    </div>
+  );
+}
