0ca1b75f4a9ef5d170262601a2b059b1e1b443ae
ꜱɢ ᴅᴀʀᴋ 神 <godwithusmustgr8@gmail.com>
2026-07-28 16:26:53 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,18 @@
+import React from 'react';
+import { ResumeProvider } from './context/ResumeContext';
+import Header from './components/Layout/Header';
+import SplitLayout from './components/Layout/SplitLayout';
+import './styles/global.css';
 
+function App() {
+  return (
+    <ResumeProvider>
+      <div className="app">
+        <Header />
+        <SplitLayout />
+      </div>
+    </ResumeProvider>
+  );
+}
+
+export default App;
