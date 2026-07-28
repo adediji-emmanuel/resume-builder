34459b8e6e8e1c68396049de4922aca399221936
ꜱɢ ᴅᴀʀᴋ 神 <godwithusmustgr8@gmail.com>
2026-07-28 16:30:47 +0100

    Update ResumePreview.jsx

--- src/components/Preview/ResumePreview.jsx
+++ src/components/Preview/ResumePreview.jsx
@@ -1,1 +1,24 @@
+import React from 'react';
+import { useResume } from '../../context/ResumeContext';
+import ClassicTemplate from './ClassicTemplate';
+import ModernTemplate from './ModernTemplate';
+import MinimalTemplate from './MinimalTemplate';
 
+const templates = {
+  classic: ClassicTemplate,
+  modern: ModernTemplate,
+  minimal: MinimalTemplate
+};
+
+export default function ResumePreview() {
+  const { state } = useResume();
+  const Template = templates[state.template] || ClassicTemplate;
+
+  return (
+    <div className="preview-container">
+      <div className={`resume-page ${state.device}`} id="resume-preview">
+        <Template data={state} />
+      </div>
+    </div>
+  );
+}
